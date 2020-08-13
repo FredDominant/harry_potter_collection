932d3d69ae766e59a229a8e723f6a3800c5a5cb7
freddominant <fredadewole@gmail.com>
2020-08-13 11:12:03 +0100

    add network layer

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+class Constants {
+  static const BASE_URL = "https://www.potterapi.com/v1";
+  static const CHARACTERS_URL = "/characters";
+  static const SORTING_HAT = "/sortingHat";
+  static const HOUSE = "/sortingHat";
+  static const SPELL = "/sortingHat";
+}
--- lib/services/harry_potter.dart
+++ lib/services/harry_potter.dart
@@ -1,9 +1,23 @@
+import 'package:http/http.dart' as http;
+
+import 'package:harry_potter_collection/models/character.dart';
+import '../constants.dart';
+
 class HarryPotter {
 
-  static const String _baseUrl = "https://www.potterapi.com/v1/";
   final String _apiKey;
 
-  HarryPotter.initialize(this._apiKey);
+  HarryPotter(this._apiKey);
 
-  
+  Future<List<Character>> fetchAllCharactersAsFuture() async {
+    final url = "${Constants.BASE_URL}/${Constants.CHARACTERS_URL}?apikey=${this
+        ._apiKey}";
+
+    final response = await http.get(url);
+    switch (response.statusCode) {
+      case 200: {
+
+      }
+    }
+  }
 }
